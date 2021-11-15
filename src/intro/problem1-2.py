d3428ac3793d143fb2ec41f8ef3d6bd3011ae065
psilons <cuirick@gmail.com>
2021-11-14 20:35:29 -0500

    note for integration by parts, add python code

--- src/intro/problem1-2.py
+++ src/intro/problem1-2.py
@@ -1,24 +1,24 @@
-# solution 1 for problem1 with formulae
+# solution 2 for problem1 with formulae
 from sympy import *
 
 x, c = symbols("x c")
 
-f1 = 2 * x * x - x ** 4
+f1 = (1 - x*x/2) ** 2
 r1 = integrate(f1, (x, 0, c), (c, 0, 1))
 print(r1)
 
-f2 = 2*x - 2*x*x + x*(2*c - c*c)
+f2 = (1 - x + c - c*c/2) ** 2
 r2 = integrate(f2, (x, c, 1), (c, 0, 1))
 print(r2)
 
-f3 = - x**4 + 6 * x**3 - 8 * x*x + (2*x - x*x)*(3 + 2*c - c*c)
+f3 = (Rational(3, 2) - 2*x + x*x/2 + c - c*c/2) ** 2  # need rational to keep final result rational
 r3 = integrate(f3, (x, 1, 1+c), (c, 0, 1))
 print(r3)
 
 res = r1 + r2 + r3
 print(res)
 
-# 2/15
-# 7/20
-# 7/45
+# 17/40
+# 23/120
+# 1/45
 # 23/36
