f07a0f3482c793a26bd1b3e0590989cbc0fe36ae
psilons <cuirick@gmail.com>
2021-12-26 01:33:02 -0500

    stats basics

---
+++ src/intro/benford_stats.py
@@ -0,0 +1,5 @@
+# https://medium.com/explorations-in-python/benfords-law-in-python-64c28a560c98
+# https://towardsdatascience.com/frawd-detection-using-benfords-law-python-code-9db8db474cf8
+# https://github.com/erdogant/benfordslaw
+# https://github.com/Liamhanninen/benfords_law
+# https://en.wikipedia.org/wiki/Benford%27s_law
